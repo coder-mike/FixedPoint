045c10ad78c2ff88653896cc58efbde1332580d1
alpylmz <ylmz.alp.e@gmail.com>
2024-02-06 21:38:35 -0500

    add new fixed point python library!

--- produce_test.py
+++ produce_test.py
@@ -1,6 +1,4 @@
-#import numpy as np
-#from FixedPoint import FXfamily, FXnum
-from spfpm.FixedPoint import FXfamily, FXnum
+from fixedpoint import FixedPoint
 from random import random
 from tqdm import tqdm
 
@@ -24,7 +22,7 @@ MAX_BOUND = 1000
 # If the line starts with a !, it means the next two numbers are the new integer and fractional bits
 # Do I need anything else?
 
-def assignment_test(fxpt):
+def assignment_test(n, m):
     # use tqdm for loop
     for _ in range(int(NUM_TESTS)):
         # produce a random number
@@ -33,141 +31,139 @@ def assignment_test(fxpt):
         # fixed_num = FXnum(num, fxpt)
         # write to file
         with open(output_file, 'a') as f:
-            f.write(f"# {num} {FXnum(num, fxpt)}\n")
+            f.write(f"# {num} {float((FixedPoint(num, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore')))}\n")
 
-def addition_test(fxpt):
+def addition_test(n, m):
     # use tqdm for loop
     for _ in range(int(NUM_TESTS)):
         # produce a random number
         num1 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         num2 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         # convert to fixed point
-        fixed_num1 = FXnum(num1, fxpt)
-        fixed_num2 = FXnum(num2, fxpt)
+        fixed_num1 = float(FixedPoint(num1, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
+        fixed_num2 = float(FixedPoint(num2, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
         # write to file
         with open(output_file, 'a') as f:
             f.write(f"+ {num1} {num2} {fixed_num1 + fixed_num2}\n")
 
-def subtraction_test(fxpt):
+def subtraction_test(n, m):
     # use tqdm for loop
     for _ in range(int(NUM_TESTS)):
         # produce a random number
         num1 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         num2 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         # convert to fixed point
-        fixed_num1 = FXnum(num1, fxpt)
-        fixed_num2 = FXnum(num2, fxpt)
+        fixed_num1 = float(FixedPoint(num1, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
+        fixed_num2 = float(FixedPoint(num2, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
         # write to file
         with open(output_file, 'a') as f:
             f.write(f"- {num1} {num2} {fixed_num1 - fixed_num2}\n")
 
-def multiplication_test(fxpt):
+def multiplication_test(n, m):
     # use tqdm for loop
     for _ in range(int(NUM_TESTS)):
         # produce a random number
         num1 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         num2 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         # convert to fixed point
-        fixed_num1 = FXnum(num1, fxpt)
-        fixed_num2 = FXnum(num2, fxpt)
+        fixed_num1 = float(FixedPoint(num1, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
+        fixed_num2 = float(FixedPoint(num2, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
         # write to file
         with open(output_file, 'a') as f:
             f.write(f"* {num1} {num2} {fixed_num1 * fixed_num2}\n")
 
-def division_test(fxpt):
+def division_test(n, m):
     # use tqdm for loop
     for _ in range(int(NUM_TESTS)):
         # produce a random number
         num1 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         num2 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         # convert to fixed point
-        fixed_num1 = FXnum(num1, fxpt)
-        fixed_num2 = FXnum(num2, fxpt)
+        fixed_num1 = float(FixedPoint(num1, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
+        fixed_num2 = float(FixedPoint(num2, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
         if(fixed_num2 == 0):
             continue
         # write to file
         with open(output_file, 'a') as f:
             f.write(f"/ {num1} {num2} {fixed_num1 / fixed_num2}\n")
 
-def unary_minus_test(fxpt):
+def unary_minus_test(n, m):
     # use tqdm for loop
     for _ in range(int(NUM_TESTS)):
         # produce a random number
         num = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         # convert to fixed point
-        fixed_num = FXnum(num, fxpt)
+        fixed_num =float( FixedPoint(num, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
         # write to file
         with open(output_file, 'a') as f:
             f.write(f"-- {num} {-fixed_num}\n")
 
-def equality_test(fxpt):
+def equality_test(n, m):
     # use tqdm for loop
     for _ in range(int(NUM_TESTS)):
         # produce a random number
         num1 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         num2 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         # convert to fixed point
-        fixed_num1 = FXnum(num1, fxpt)
-        fixed_num2 = FXnum(num2, fxpt)
+        fixed_num1 = float(FixedPoint(num1, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
+        fixed_num2 = float(FixedPoint(num2, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
         # write to file
         with open(output_file, 'a') as f:
             f.write(f"== {num1} {num2} {fixed_num1 == fixed_num2}\n")
 
-def less_than_test(fxpt):
+def less_than_test(n, m):
     # use tqdm for loop
     for _ in range(int(NUM_TESTS)):
         # produce a random number
         num1 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         num2 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         # convert to fixed point
-        fixed_num1 = FXnum(num1, fxpt)
-        fixed_num2 = FXnum(num2, fxpt)
+        fixed_num1 = float(FixedPoint(num1, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
+        fixed_num2 = float(FixedPoint(num2, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
         # write to file
         with open(output_file, 'a') as f:
             f.write(f"< {num1} {num2} {fixed_num1 < fixed_num2}\n")
 
-def greater_than_test(fxpt):
+def greater_than_test(n, m):
     # use tqdm for loop
     for _ in range(int(NUM_TESTS)):
         # produce a random number
         num1 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         num2 = (random() * (MAX_BOUND - MIN_BOUND)) + MIN_BOUND
         # convert to fixed point
-        fixed_num1 = FXnum(num1, fxpt)
-        fixed_num2 = FXnum(num2, fxpt)
+        fixed_num1 = float(FixedPoint(num1, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
+        fixed_num2 = float(FixedPoint(num2, True, m, n, overflow='wrap', rounding='down', overflow_alert='ignore'))
         # write to file
         with open(output_file, 'a') as f:
             f.write(f"> {num1} {num2} {fixed_num1 > fixed_num2}\n")
 
 
-def test_num(fxpt, N, M):
+def test_num(N, M):
     with open(output_file, 'a') as f:
         f.write(f"! {N} {M}\n")
-    assignment_test(fxpt)
-    addition_test(fxpt)
-    subtraction_test(fxpt)
-    multiplication_test(fxpt)
-    division_test(fxpt)
-    unary_minus_test(fxpt)
-    equality_test(fxpt)
-    less_than_test(fxpt)
-    greater_than_test(fxpt)
+    assignment_test(N, M)
+    addition_test(N, M)
+    subtraction_test(N, M)
+    multiplication_test(N, M)
+    #division_test(N, M)
+    unary_minus_test(N, M)
+    equality_test(N, M)
+    less_than_test(N, M)
+    greater_than_test(N, M)
 
 
 #N=6
 #custom_fxpt = FXfamily(n_bits=32, n_intbits=N)
-for N in tqdm(range(1, 33)):
-    for M in tqdm(range(1, 33)):
+for N in tqdm(range(8, 9)):
+    for M in tqdm(range(8, 9)):
         #print("N: ", N, "M: ", M)
-        custom_fxpt = FXfamily(n_bits=N, n_intbits=M)
-        test_num(custom_fxpt, N, M)
+        test_num(N, M)
 
 
 """
-custom_fixed = FXfamily(n_bits=1, n_intbits=1)
-num1 = FXnum(-2689879756739741, custom_fixed)
-print(num1)
-num2 = FXnum(0.5, custom_fixed)
-print(num2)
+# example
+custom_fixed = FixedPoint(34, True, m=8, n=8, overflow='wrap', rounding='down', overflow_alert='ignore')
+print(float(custom_fixed))
+num2 = FixedPoint(5, True, m=8, n=8)
+print(float(num2))
 """
-
